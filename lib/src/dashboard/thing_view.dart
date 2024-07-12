bf740a3600640e1a96b0e3126d16d7c3b4ecb042
Eldon Lucas (2021) ZKAC267 <lucas.eldon.2021@live.rhul.ac.uk>
2024-07-12 12:15:13 +0100

    Removed unused import

--- lib/src/dashboard/thing_view.dart
+++ lib/src/dashboard/thing_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:rank_everything/src/dashboard/thing.dart';
 import 'package:rank_everything/src/dashboard/thing_provider.dart';
