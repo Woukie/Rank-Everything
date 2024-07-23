ca8978c85711acc99d0d21235a0c101e39e52951
Woukie <lucaseldon@live.com>
2024-07-23 17:59:16 +0100

    Updated style of stats page

--- lib/src/stats/stats_view.dart
+++ lib/src/stats/stats_view.dart
@@ -25,133 +25,26 @@ class _StatsViewState extends State<StatsView> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: searchController,
-                  onChanged: searchProvider.search,
-                  decoration: InputDecoration(
-                    prefixIcon: const Padding(
-                      padding: EdgeInsets.only(left: 6),
-                      child: Icon(Icons.search),
-                    ),
-                    filled: true,
-                    border: const OutlineInputBorder(),
-                    suffixIcon: searchController.text == ''
-                        ? Container()
-                        : Padding(
-                            padding: const EdgeInsets.only(right: 6),
-                            child: IconButton(
-                              onPressed: () {
-                                searchController.clear();
-                                searchProvider.search('');
-                              },
-                              icon: const Icon(Icons.clear),
-                            ),
-                          ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 6),
-                child: IconButton(
-                  onPressed: () {
-                    searchProvider.refresh();
-                  },
-                  icon: const Icon(Icons.refresh),
-                ),
-              )
-            ],
+          SearchBar(
+            searchController: searchController,
+            searchProvider: searchProvider,
           ),
+          AscendingSelector(
+            searchProvider: searchProvider,
+            searchController: searchController,
+          ),
+          ThingList(searchProvider: searchProvider),
           Padding(
-            padding: const EdgeInsets.fromLTRB(6, 14, 6, 6),
-            child: Wrap(
-              alignment: WrapAlignment.center,
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                Text(
-                  "Showing the 10 ",
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                DropdownButton(
-                  value: searchProvider.ascending,
-                  style: Theme.of(context).textTheme.titleMedium,
-                  selectedItemBuilder: (context) => [
-                    // Yes it has to be like this to avoid the tiny gap between the
-                    // arrow and the text
-                    // The width of the dropdown is the same as the smallest item, and
-                    // we have two because it needs to be the same length as values
-                    Text(searchProvider.ascending ? "worst" : "best"),
-                    Text(searchProvider.ascending ? "worst" : "best")
-                  ],
-                  isDense: true,
-                  items: const [
-                    DropdownMenuItem(
-                      value: false,
-                      child: Text("best"),
-                    ),
-                    DropdownMenuItem(
-                      value: true,
-                      child: Text("worst"),
-                    )
-                  ],
-                  onChanged: (value) {
-                    if (value == null) return;
-
-                    searchProvider.setAscending(value);
-                    searchProvider.search(
-                      searchController.text,
-                      override: true,
-                    );
-                  },
-                ),
-                // Split the last two for some jank text wrapping
-                Text(
-                  " things ",
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Text(
-                  "ever!",
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-              ],
-            ),
-          ),
-          const Divider(),
-          Expanded(
-            child: Card(
-              child: ListView.builder(
-                itemCount: searchProvider.searchResults.length,
-                itemBuilder: (context, index) {
-                  Thing thing = searchProvider.searchResults[index];
-
-                  return ListTile(
-                    titleAlignment: ListTileTitleAlignment.center,
-                    title: Text(
-                      thing.name,
-                    ),
-                    leading: Text(
-                      (index + 1).toString(),
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                    trailing: Text(
-                      thing.getPercentage().toString(),
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    key: Key(index.toString()),
-                  );
-                },
-              ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(bottom: 3),
+            padding: const EdgeInsets.symmetric(vertical: 10),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(child: Divider()),
-                Text("Missing something?"),
-                Expanded(child: Divider()),
+                const Expanded(child: Divider()),
+                Text(
+                  "Missing something?",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                const Expanded(child: Divider()),
               ],
             ),
           ),
@@ -170,6 +63,175 @@ class _StatsViewState extends State<StatsView> {
           )
         ],
       ),
+    );
+  }
+}
+
+class ThingList extends StatelessWidget {
+  const ThingList({
+    super.key,
+    required this.searchProvider,
+  });
+
+  final SearchProvider searchProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Card(
+        margin: EdgeInsets.zero,
+        child: ListView.builder(
+          itemCount: searchProvider.searchResults.length,
+          itemBuilder: (context, index) {
+            Thing thing = searchProvider.searchResults[index];
+
+            return ListTile(
+              titleAlignment: ListTileTitleAlignment.center,
+              title: Text(
+                thing.name,
+              ),
+              leading: Text(
+                (index + 1).toString(),
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              trailing: Text(
+                thing.getPercentage().toString(),
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              key: Key(index.toString()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class AscendingSelector extends StatelessWidget {
+  const AscendingSelector({
+    super.key,
+    required this.searchProvider,
+    required this.searchController,
+  });
+
+  final SearchProvider searchProvider;
+  final TextEditingController searchController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: [
+          const Expanded(child: Divider()),
+          Wrap(
+            alignment: WrapAlignment.center,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            children: [
+              Text(
+                "Showing the 10 ",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              DropdownButton(
+                value: searchProvider.ascending,
+                style: Theme.of(context).textTheme.titleMedium,
+                selectedItemBuilder: (context) => [
+                  // Yes it has to be like this to avoid the tiny gap between the
+                  // arrow and the text
+                  // The width of the dropdown is the same as the smallest item, and
+                  // we have two because it needs to be the same length as values
+                  Text(searchProvider.ascending ? "worst" : "best"),
+                  Text(searchProvider.ascending ? "worst" : "best")
+                ],
+                isDense: true,
+                items: const [
+                  DropdownMenuItem(
+                    value: false,
+                    child: Text("best"),
+                  ),
+                  DropdownMenuItem(
+                    value: true,
+                    child: Text("worst"),
+                  )
+                ],
+                onChanged: (value) {
+                  if (value == null) return;
+
+                  searchProvider.setAscending(value);
+                  searchProvider.search(
+                    searchController.text,
+                    override: true,
+                  );
+                },
+              ),
+              // Split the last two for some jank text wrapping
+              Text(
+                " things ",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Text(
+                "ever!",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ],
+          ),
+          const Expanded(child: Divider()),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    super.key,
+    required this.searchController,
+    required this.searchProvider,
+  });
+
+  final TextEditingController searchController;
+  final SearchProvider searchProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: searchController,
+            onChanged: searchProvider.search,
+            decoration: InputDecoration(
+              prefixIcon: const Padding(
+                padding: EdgeInsets.only(left: 6),
+                child: Icon(Icons.search),
+              ),
+              filled: true,
+              border: const OutlineInputBorder(),
+              suffixIcon: searchController.text == ''
+                  ? Container()
+                  : Padding(
+                      padding: const EdgeInsets.only(right: 6),
+                      child: IconButton(
+                        onPressed: () {
+                          searchController.clear();
+                          searchProvider.search('');
+                        },
+                        icon: const Icon(Icons.clear),
+                      ),
+                    ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 6),
+          child: IconButton(
+            onPressed: () {
+              searchProvider.refresh();
+            },
+            icon: const Icon(Icons.refresh),
+          ),
+        )
+      ],
     );
   }
 }
